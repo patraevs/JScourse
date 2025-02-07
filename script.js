fc7dd06d5e89f607fe87984a22b660a074f6e7b8
patraevs <nvnesh@yandex.ru>
2025-02-07 14:52:43 +0300

    test

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-alert("hello my script")
-console.log("hello console")
+alert("hello my script");
+console.log("hello console");
+console.log("test");
+let title = 1;
+let screens = 2;
+let screenPrice = 3;
+let rollback = 4;
+let fullPrice = 5;
+let adaptive6 = 6;
