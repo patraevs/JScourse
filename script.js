3d5eed9bb4525237eeb826e84d883fb87475a600
patraevs <nvnesh@yandex.ru>
2025-02-07 16:35:21 +0300

    dz1

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-alert("hello my script")
-
-const a = 1;
-const b = 2;
+alert("hello my script");
+console.log("hello console");
+console.log("test");
+let title = 1;
+let screens = 2;
+let screenPrice = 3;
+let rollback = 4;
+let fullPrice = 5;
+let adaptive6 = 6;
