78bbe6bfc4ec369edf031d51ff8917ac41eea235
patraevs <nvnesh@yandex.ru>
2025-02-10 00:08:33 +0300

    Типы данных, операторы, методы и свойства

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-alert("hello my script");
-console.log("hello console");
-console.log("test");
-let title = 1;
-let screens = 2;
-let screenPrice = 3;
-let rollback = 4;
-let fullPrice = 5;
-let adaptive6 = 6;
+let title = "project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 7;
+let rollback = 69;
+let fullPrice = 300;
+let adaptive6 = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive6);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей. " + " Стоимость разработки сайта " + fullPrice + " рублей.");
+console.log(screens.toLowerCase().split());
+console.log((fullPrice * (rollback / 100)) / (fullPrice / 100) + "%");
+
+
+
