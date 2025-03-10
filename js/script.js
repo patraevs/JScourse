1ca903a26a97a05c1a5809fae5e2d1a590c81441
patraevs <nvnesh@yandex.ru>
2025-03-10 12:31:34 +0300

    Работа с DOM

--- js/script.js
+++ js/script.js
@@ -107,35 +107,69 @@ const appData = {
     }
 }
 
-appData.start()
+// appData.start()
 
-const titleLayout = document.getElementsByTagName("h1")
-console.log(titleLayout[0]);
+// const titleLayout = document.getElementsByTagName("h1")
+// console.log(titleLayout[0]);
 
-const buttons = document.getElementsByClassName("handler_btn")
-console.log(buttons);
+// const buttons = document.getElementsByClassName("handler_btn")
+// console.log(buttons);
 
-const button = document.querySelector(".screen-btn")
-console.log(button);
+// const button = document.querySelector(".screen-btn")
+// console.log(button);
 
-const elementsPercent = document.querySelectorAll(".other-items + .percent")
-console.log(elementsPercent);
+// const elementsPercent = document.querySelectorAll(".other-items + .percent")
+// console.log(elementsPercent);
 
-const elementsNumber = document.querySelectorAll(".other-items + .number")
-console.log(elementsNumber);
+// const elementsNumber = document.querySelectorAll(".other-items + .number")
+// console.log(elementsNumber);
 
-const input = document.querySelector(".rollback input[type=range]")
-console.log(input);
+// const input = document.querySelector(".rollback input[type=range]")
+// console.log(input);
 
-const span = document.querySelector(".rollback .range-value")
-console.log(span);
+// const span = document.querySelector(".rollback .range-value")
+// console.log(span);
 
-const allInput = document.getElementsByClassName("total-input")
-console.log(allInput[0]);
-console.log(allInput[1]);
-console.log(allInput[2]);
-console.log(allInput[3]);
-console.log(allInput[4]);
+// const allInput = document.getElementsByClassName("total-input")
+// console.log(allInput[0]);
+// console.log(allInput[1]);
+// console.log(allInput[2]);
+// console.log(allInput[3]);
+// console.log(allInput[4]);
 
-let allBlock = document.querySelectorAll(".screen")
-console.log(allBlock);
+// let allBlock = document.querySelectorAll(".screen")
+// console.log(allBlock);
+
+const books = document.querySelectorAll('.book')
+const elems = document.getElementsByTagName("li")
+const titles = document.getElementsByTagName('h2')
+
+
+console.log(books);
+console.log(elems);
+console.log(titles);
+
+
+books[0].before(books[1])
+books[5].after(books[2])
+books[3].before(books[4])
+
+const cloneTitle = titles[2].cloneNode(true)
+cloneTitle.innerHTML = '<a href="https://github.com/azat-io/you-dont-know-js-ru/blob/master/this%20%26%20object%20prototypes/README.md#you-dont-know-js-this--object-prototypes" target="_blank">Книга 3. this и Прототипы Объектов</a>'
+titles[2].replaceWith(cloneTitle);
+
+const marketing = document.querySelectorAll(".adv")
+marketing[0].remove()
+
+elems[16].before(elems[8])
+elems[9].before(elems[11])
+elems[10].before(elems[13])
+
+elems[38].before(elems[45])
+elems[42].before(elems[39])
+elems[45].before(elems[42])
+
+const newElem = document.createElement('li')
+newElem.textContent = "Глава 8: За пределами ES6"
+books[2].append(newElem)
+elems[56].before(elems[57])
